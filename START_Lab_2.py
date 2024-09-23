cbcc2a8908af73b8ff97fe38b438fda38c400baa
Amanpreet-coder <amanpreet.chem@gmail.com>
2024-09-23 04:07:38 -0600

    Lab2 Assignment

--- START_Lab_2.py
+++ START_Lab_2.py
@@ -17,22 +17,32 @@ def lab2Question1(word):
 def lab2Question2(number_val):
     # Create a function that takes in a number
     # Return a list of the fibonacci sequence up to that number
-    series = [0,1]
-    a = series[0]
-    b = series[1]
-    for i in range(3, number_val+1):
-        c = a + b
-        series.append(c)
-        a = b
-        b = c
-    return series
-    pass
+    if(number_val <= 0):
+        return []
+    elif (number_val == 1):
+        return [0]
+    elif (number_val == 2):
+        return [0,1]
+    else:
+        series = [0,1]
+        a = series[0]
+        b = series[1]
+        for i in range(3, number_val+1):
+            c = a + b
+            a = b
+            b = c
+            if (c < number_val):
+                series.append(c)
+            else:
+                break
+        return series
+        pass
 
 def lab2Question3(str1, str2):
     # Create a function that takes in two strings - str1 and str2
     # Return the number of times str2 appears in str1
     # For example if str1 = "coding is cool" and str2 = "co" then output should be 2.
-    return str1.count(str2)
+    return str1.lower().count(str2.lower())
     pass
 
 def lab2Question4(list1, list2):
